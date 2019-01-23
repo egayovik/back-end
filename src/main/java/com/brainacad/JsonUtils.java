d08ae63409471f87acba272a4210ea3a849cf1a3
student <egayovik@gmail.com>
2019-01-23 20:41:52 +0200

    task_2

--- src/main/java/com/brainacad/JsonUtils.java
+++ src/main/java/com/brainacad/JsonUtils.java
@@ -1,29 +1,37 @@
 package com.brainacad;
 
+import com.jayway.jsonpath.JsonPath;
+
+import java.awt.*;
+
 public class JsonUtils {
     //TODO: Используя библиотеку com.jayway.jsonpath (Maven) напишите метод извлечения строки из JSON по JSON Path:
 
-    public static String stringFromJSONByPath(String json, String jsonPath){
+    public static String stringFromJSONByPath(String json, String jsonPath) {
+
+        return JsonPath.read(json, jsonPath);
 
     }
 
 
     //TODO: Используя библиотеку com.jayway.jsonpath (Maven) напишите метод извлечения int из JSON по JSON Path:
-    /*
+
     public static int intFromJSONByPath(String json, String jsonPath){
+        return JsonPath.read(json, jsonPath);
+
     }
-    */
 
     //TODO: Используя библиотеку com.jayway.jsonpath (Maven) напишите метод извлечения double из JSON по JSON Path:
-    /*
+
     public static double doubleFromJSONByPath(String json, String jsonPath){
+        return JsonPath.read(json, jsonPath);
     }
-    */
+
 
     //TODO: Используя библиотеку com.jayway.jsonpath (Maven) напишите метод извлечения списка (List) из JSON по JSON Path:
-    /*
+
     public static List listFromJSONByPath(String json, String jsonPath){
+        return JsonPath.read(json, jsonPath);
     }
-    */
 
 }
